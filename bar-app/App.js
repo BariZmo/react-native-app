4f404a003bc089200234c36deb441f3667420f02
Mantulis00 <mantas.radzevicius1@gmail.com>
2020-05-07 10:34:06 +0300

    Init

--- bar-app/App.js
+++ bar-app/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [out, set] = useState("text to be set");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ padding: 30 }}>
+      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <TextInput
+          placeholder="xd"
+          style={{ borderColor: "black", borderBottomWidth: 1, padding: 10 }}
+        />
+        <Button title="Add" />
+      </View>
     </View>
   );
 }
@@ -12,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
