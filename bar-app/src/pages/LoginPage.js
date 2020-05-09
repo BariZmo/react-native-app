4afc8a38ed09c5319a71fc0c3664c11d31014e4b
z7z53 <64813972+z7z53@users.noreply.github.com>
2020-05-09 17:33:23 +0300

    add text input fields

--- bar-app/src/pages/LoginPage.js
+++ bar-app/src/pages/LoginPage.js
@@ -1,31 +1,69 @@
-import React from "react";
-import { Text, View, Button, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Text, View, StyleSheet, TextInput } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function ({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Sample LoginPage text!</Text>
-      <Button
-        title="Go to User"
-        onPress={() => navigation.navigate("UserPage")}
+      <Text style={styles.title}>Sveiki!</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Jūsų el. paštas"
+        placeholderTextColor="grey"
       />
-      <Button
-        title="Go to Staff"
-        onPress={() => navigation.navigate("StaffPage")}
+      <TextInput
+        style={styles.input}
+        placeholder="Jūsų slaptažodis"
+        placeholderTextColor="grey"
       />
-      <Button
-        title="Go to Admin"
-        onPress={() => navigation.navigate("AdminPage")}
-      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {} /*navigation.navigate("UserPage")*/}
+      >
+        <Text style={styles.buttonText}>Tęsti</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+    margin: 30,
+    width: 200,
+    textAlign: "center",
+    color: "white",
+    textShadowRadius: 3,
+    textShadowColor: "black",
+    textShadowOffset: { width: 1, height: 1 },
+  },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#809fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  input: {
+    borderWidth: 2,
+    borderColor: "white",
+    backgroundColor: "lightgrey",
+    padding: 8,
+    margin: 10,
+    width: 200,
+    borderRadius: 10,
+  },
+  button: {
+    alignItems: "center",
+    width: 100,
+    padding: 10,
+    margin: 20,
+    backgroundColor: "#00aeef",
+    borderColor: "white",
+    borderWidth: 3,
+    borderRadius: 15,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "white",
   },
 });
