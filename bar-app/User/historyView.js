2c15bbbe0f86c7491ede790b16556a5313e415f4
Mantulis00 <mantas.radzevicius1@gmail.com>
2020-05-11 11:25:53 +0300

    #History, expenses 0.05a moving on

--- bar-app/User/historyView.js
+++ bar-app/User/historyView.js
@@ -4,7 +4,7 @@ import {
   Text,
   View,
   Button,
-  TextInput,
+  TouchableOpacity,
   FlatList,
   TextComponent,
   Modal,
@@ -13,12 +13,6 @@ import {
 // *sort by date
 
 export default function (props) {
-  const [barInfo, setBarInfo] = useState({
-    id: 88,
-    date: "2020-14-08",
-    bar: "Pas Vytauta",
-    money: 0,
-  });
   // temp, sb from props
   const [infoArray, setInfoArray] = useState([
     { id: 1, date: "2020 - 08 - 08", bar: "stikliai", money: 100 },
@@ -30,9 +24,9 @@ export default function (props) {
   });
   // temp, sb from props
   const [spendInfo, setSpendInfo] = useState([
-    { id: 1, item: "surelis", price: 20.21 },
-    { id: 1, item: "jogurtas", price: 14.87 },
-    { id: 2, item: "tortas", price: 11.27 },
+    { identityid: 1, id: 1, item: "surelis", price: 20.21 },
+    { identityid: 2, id: 1, item: "jogurtas", price: 14.87 },
+    { identityid: 3, id: 2, item: "tortas", price: 11.27 },
   ]);
 
   const [modalVisibility, setModalVisibility] = useState(false);
@@ -42,31 +36,29 @@ export default function (props) {
     setModalVisibility(true);
   };
 
-  // renders all visits to bars, bar - visit info - sum spent <-- tbd
-
-  /// learn 2 update vars
   const calcSum = (IDE) => {
-    console.log(IDE);
     var sum = 0;
-
     spendInfo.map((infoElement) =>
       infoElement.id === IDE ? (sum += infoElement.price) : null
     );
 
-    infoArray.map((infoElement) => (infoElement.id === IDE ? barInfo : null)); // <-------------- fix
-    console.log(sum);
+    var index = infoArray.findIndex((obj) => obj.id === IDE);
+    var updatedInfo = infoArray[index];
+    updatedInfo.money = sum;
   };
 
   function Item({ infoElement }) {
-    console.log(infoElement.money);
     return (
       <View>
-        <View style={styles.row}>
+        <View style={styles.spacer}></View>
+        <TouchableOpacity
+          style={styles.touchable}
+          onPress={() => showDetails(infoElement.id)}
+        >
           <Text>Bar: {infoElement.bar}</Text>
           <Text>DATE: {infoElement.date}</Text>
           <Text>Money spent: {infoElement.money}</Text>
-        </View>
-        <Button title="Spendings" onPress={() => showDetails(infoElement.id)} />
+        </TouchableOpacity>
       </View>
     );
   }
@@ -83,7 +75,9 @@ export default function (props) {
       </View>
     );
   }
-  calcSum(1);
+
+  infoArray.map((info) => calcSum(info.id));
+
   return (
     <View style={styles.main}>
       <Modal visible={modalVisibility} animationType="slide">
@@ -91,7 +85,7 @@ export default function (props) {
           <FlatList
             data={spendInfo}
             renderItem={({ item }) => <VisitInfo infoElement={item} />}
-            keyExtractor={(item) => item.id}
+            keyExtractor={(item) => item.identityid}
           />
           <Button title="go back" onPress={() => setModalVisibility(false)} />
         </View>
@@ -110,6 +104,19 @@ const styles = StyleSheet.create({
   main: {
     height: "50%",
   },
+  touchable: {
+    width: "100%",
+    flexDirection: "row",
+    paddingVertical: "5%",
+    backgroundColor: "teal",
+    textDecorationColor: "yellow",
+    textShadowColor: "red",
+    textShadowRadius: 1,
+    borderColor: "green",
+    borderRadius: 10,
+    borderStyle: "solid",
+    borderWidth: 2,
+  },
   list: {
     width: "100%",
   },
@@ -118,6 +125,9 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
 
     // width: "100%",
+  },
+  spacer: {
+    paddingTop: "5%",
   },
 
   elementID: {
