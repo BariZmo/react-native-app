991133749af83cdf99cd084e221f59d8c2555cb2
z7z53 <64813972+z7z53@users.noreply.github.com>
2020-05-11 20:20:51 +0300

    add extended information menu

--- bar-app/src/pages/staff-pages/SampleReservations.js
+++ bar-app/src/pages/staff-pages/SampleReservations.js
@@ -2,6 +2,8 @@ exports.SampleReservations = [
   {
     name: "Antanas",
     surname: "Smetona",
+    number: "860000001",
+    email: "antanas@smetona.com",
     otherPeople: 0,
     date: "2020-05-11 23:55",
     id: 0,
@@ -9,6 +11,8 @@ exports.SampleReservations = [
   {
     name: "Aleksandras",
     surname: "Stulginskis",
+    number: "860000002",
+    email: "aleksandras@stulginskis.com",
     otherPeople: 2,
     date: "2020-05-13 22:10",
     id: 1,
@@ -16,6 +20,8 @@ exports.SampleReservations = [
   {
     name: "Kazys",
     surname: "Grinius",
+    number: "860000003",
+    email: "kazys@grinius.com",
     otherPeople: 1,
     date: "2020-05-12 20:20",
     id: 2,
@@ -23,6 +29,8 @@ exports.SampleReservations = [
   {
     name: "Jonas",
     surname: "Staugaitis",
+    number: "860000004",
+    email: "jonas@staugaitis.com",
     otherPeople: 4,
     date: "2020-05-10 00:10",
     id: 3,
@@ -30,6 +38,8 @@ exports.SampleReservations = [
   {
     name: "Antanas",
     surname: "Merkys",
+    number: "860000005",
+    email: "antanas@merkys.com",
     otherPeople: 0,
     date: "2020-05-14 20:40",
     id: 4,
@@ -37,6 +47,8 @@ exports.SampleReservations = [
   {
     name: "Justas",
     surname: "Paleckis",
+    number: "860000006",
+    email: "justas@paleckis.com",
     otherPeople: 3,
     date: "2020-05-17 21:15",
     id: 5,
@@ -44,6 +56,8 @@ exports.SampleReservations = [
   {
     name: "Algirdas",
     surname: "Brazauskas",
+    number: "860000007",
+    email: "algirdas@brazauskas.com",
     otherPeople: 2,
     date: "2020-05-11 20:45",
     id: 6,
@@ -51,6 +65,8 @@ exports.SampleReservations = [
   {
     name: "Valdas",
     surname: "Adamkus",
+    number: "860000008",
+    email: "valdas@adamkus.com",
     otherPeople: 1,
     date: "2020-05-10 22:20",
     id: 7,
@@ -58,6 +74,8 @@ exports.SampleReservations = [
   {
     name: "Rolandas",
     surname: "Paksas",
+    number: "860000009",
+    email: "rolandas@paksas.com",
     otherPeople: 1,
     date: "2020-05-09 20:05",
     id: 8,
@@ -65,6 +83,8 @@ exports.SampleReservations = [
   {
     name: "Artūras",
     surname: "Paulauskas",
+    number: "860000010",
+    email: "arturas@paulauskas.com",
     otherPeople: 3,
     date: "2020-05-13 21:00",
     id: 9,
@@ -72,6 +92,8 @@ exports.SampleReservations = [
   {
     name: "Dalia",
     surname: "Grybauskaitė",
+    number: "860000011",
+    email: "dalia@grybauskaite.com",
     otherPeople: 0,
     date: "2020-05-12 23:20",
     id: 10,
@@ -79,6 +101,8 @@ exports.SampleReservations = [
   {
     name: "Gitanas",
     surname: "Nausėda",
+    number: "860000012",
+    email: "gitanas@nauseda.com",
     otherPeople: 7,
     date: "2020-05-11 21:55",
     id: 11,
--- bar-app/src/pages/staff-pages/UnapprovedReservationPage.js
+++ bar-app/src/pages/staff-pages/UnapprovedReservationPage.js
@@ -1,17 +1,34 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet, BackHandler, FlatList } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import {
+  Text,
+  View,
+  StyleSheet,
+  BackHandler,
+  FlatList,
+  Modal,
+  TouchableOpacity,
+  TouchableHighlight,
+} from "react-native";
+import {} from "react-native-gesture-handler";
 import { SampleReservations } from "./SampleReservations";
 
 export default function () {
   BackHandler.addEventListener("hardwareBackPress", () => true);
 
   const [reservations, setReservations] = useState(SampleReservations);
+  // selectedItem is identified by id
+  const [selectedItem, setSelectedItem] = useState(-1);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const removeHandler = (id) => {
     setReservations((prevReservations) => {
       return prevReservations.filter((reservation) => reservation.id != id);
     });
+  };
+
+  const openHandler = (id) => {
+    setSelectedItem(id);
+    setMenuOpen(true);
   };
 
   return (
@@ -23,17 +40,79 @@ export default function () {
           Viso {reservations.length} nepatvirtinta(-os) rezervacijos:
         </Text>
       )}
+      <Modal
+        style={modalStyles.modal}
+        transparent={true}
+        visible={menuOpen}
+        onRequestClose={() => {
+          setMenuOpen(false);
+        }}
+      >
+        <View style={modalStyles.modalBase}>
+          <TouchableHighlight
+            style={modalStyles.continueButton}
+            activeOpacity={0.5}
+            onPress={() => {
+              setMenuOpen(false);
+            }}
+          >
+            <Text>Tęsti</Text>
+          </TouchableHighlight>
+          <Text style={modalStyles.entry}>
+            Atvykimo data: {getReservation(reservations, selectedItem).date}
+          </Text>
+          <Text style={modalStyles.entry}>
+            El. paštas: {getReservation(reservations, selectedItem).email}
+          </Text>
+          <Text style={modalStyles.entry}>
+            Tel. nr.: {getReservation(reservations, selectedItem).number}
+          </Text>
+          <Text style={modalStyles.entry}>
+            Viso lankytojų:{" "}
+            {getReservation(reservations, selectedItem).otherPeople + 1}
+          </Text>
+          <Text style={modalStyles.name}>
+            {getReservation(reservations, selectedItem).name +
+              " " +
+              getReservation(reservations, selectedItem).surname}
+          </Text>
+          <Text style={modalStyles.title}>
+            Išsami informacija apie lankytoją:
+          </Text>
+        </View>
+      </Modal>
       <FlatList
         data={reservations}
         renderItem={({ item }) => (
-          <ListItem item={item} handler={removeHandler} />
+          <ListItem
+            item={item}
+            removeHandler={removeHandler}
+            openHandler={openHandler}
+          />
         )}
       ></FlatList>
     </View>
   );
 }
 
-function ListItem({ item, handler }) {
+function getReservation(reservations, id) {
+  reservation = reservations.find((res) => {
+    return res.id == id;
+  });
+  if (reservation != undefined) return reservation;
+  else
+    return {
+      name: "",
+      surname: "",
+      number: "",
+      email: "",
+      otherPeople: 0,
+      date: "",
+      id: 0,
+    };
+}
+
+function ListItem({ item, removeHandler, openHandler }) {
   return (
     <View style={itemStyles.item}>
       <View style={itemStyles.firstRow}>
@@ -46,7 +125,7 @@ function ListItem({ item, handler }) {
           style={[itemStyles.acceptButton, itemStyles.buttonStyle]}
           // TODO: move accepted reservation to "all reservations"
           onPress={() => {
-            handler(item.id);
+            removeHandler(item.id);
           }}
         >
           <Text>Priimti</Text>
@@ -57,6 +136,9 @@ function ListItem({ item, handler }) {
         <TouchableOpacity
           activeOpacity={0.5}
           style={[itemStyles.itemButton, itemStyles.buttonStyle]}
+          onPress={() => {
+            openHandler(item.id);
+          }}
         >
           <Text>Daugiau</Text>
         </TouchableOpacity>
@@ -64,7 +146,7 @@ function ListItem({ item, handler }) {
           activeOpacity={0.5}
           style={[itemStyles.cancelButton, itemStyles.buttonStyle]}
           onPress={() => {
-            handler(item.id);
+            removeHandler(item.id);
           }}
         >
           <Text>Atšaukti</Text>
@@ -73,6 +155,47 @@ function ListItem({ item, handler }) {
     </View>
   );
 }
+
+const modalStyles = StyleSheet.create({
+  modal: {},
+  modalBase: {
+    backgroundColor: "#EEEEEE",
+    flex: 1,
+    borderRadius: 20,
+    marginTop: "30%",
+    marginBottom: "40%",
+    marginLeft: "5%",
+    marginRight: "5%",
+    flexDirection: "column-reverse",
+    justifyContent: "space-between",
+  },
+  continueButton: {
+    alignSelf: "flex-end",
+    margin: 10,
+    padding: 10,
+    backgroundColor: "#DDDDDD",
+    borderRadius: 15,
+    width: 75,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 16,
+    padding: 5,
+    margin: 10,
+  },
+  entry: {
+    fontSize: 16,
+    padding: 5,
+    margin: 5,
+    backgroundColor: "white",
+  },
+  name: {
+    fontSize: 20,
+    padding: 5,
+    margin: 5,
+    fontWeight: "bold",
+  },
+});
 
 const itemStyles = StyleSheet.create({
   buttonStyle: {
@@ -108,11 +231,12 @@ const itemStyles = StyleSheet.create({
   },
   name: {
     flex: 1,
-    fontSize: 18,
+    fontSize: 16,
+    fontWeight: "bold",
     padding: 5,
   },
   detail: {
-    flex: 2,
+    flex: 1.5,
     fontSize: 14,
     padding: 5,
   },
