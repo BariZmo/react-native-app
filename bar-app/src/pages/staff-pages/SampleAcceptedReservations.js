5a33d1aabae2b49c4c206576a8d3bac4744fa6fc
z7z53 <64813972+z7z53@users.noreply.github.com>
2020-05-12 03:51:51 +0300

    display accepted reservations in a seperate list

---
+++ bar-app/src/pages/staff-pages/SampleAcceptedReservations.js
@@ -0,0 +1,29 @@
+exports.SampleAcceptedReservations = [
+  {
+    name: "Kristijonas",
+    surname: "Donelaitis",
+    number: "860000100",
+    email: "kristijonas@donelaitis.com",
+    otherPeople: 4,
+    date: "2020-05-14 19:25",
+    id: 0,
+  },
+  {
+    name: "Martynas",
+    surname: "Mažvydas",
+    number: "860000200",
+    email: "martynas@mazvydas.com",
+    otherPeople: 0,
+    date: "2020-05-14 20:45",
+    id: 1,
+  },
+  {
+    name: "Jonas",
+    surname: "Biliūnas",
+    number: "860000303",
+    email: "jonas@biliunas.com",
+    otherPeople: 1,
+    date: "2020-05-15 20:50",
+    id: 2,
+  },
+];
