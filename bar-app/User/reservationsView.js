8e6ef235d304c81a8a66c0d3c9d02f6dc68303a8
Mantulis00 <mantas.radzevicius1@gmail.com>
2020-05-13 16:41:01 +0300

    #swipes

--- bar-app/User/reservationsView.js
+++ bar-app/User/reservationsView.js
@@ -8,6 +8,7 @@ import {
   FlatList,
   TextComponent,
   Modal,
+  Animated,
 } from "react-native";
 import { GestureHandler } from "expo";
 import Swipeable from "react-native-gesture-handler/Swipeable";
@@ -26,16 +27,64 @@ export default function (props) {
   const [modalVisibility, setModalVisibility] = useState(false);
 
   const LeftActions = (progress, dragX) => {
+    const scale = dragX.interpolate({
+      inputRange: [0, 100],
+      outputRange: [0, 1],
+      extrapolate: "clamp",
+    });
     return (
       <View style={styles.leftBack}>
-        <Text style={styles.leftText}>REMOVE</Text>
+        <Animated.Text style={[styles.leftText, { transform: [{ scale }] }]}>
+          Daugiau informacijos
+        </Animated.Text>
+      </View>
+    );
+  };
+  const LeftActionOpen = () => {
+    console.log("more info");
+  };
+
+  const RightActions = ({ progress, dragX, index }) => {
+    const scale = dragX.interpolate({
+      inputRange: [-100, 0],
+      outputRange: [1, 0],
+      extrapolate: "clamp",
+    });
+    return (
+      <View style={styles.rightBack}>
+        <TouchableOpacity>
+          <Animated.Text
+            style={[styles.rightText, { transform: [{ scale }] }]}
+            onPress={() => DeleteAction(index)}
+          >
+            Atšaukti
+          </Animated.Text>
+        </TouchableOpacity>
       </View>
     );
   };
 
-  function Item({ infoElement }) {
+  const DeleteAction = (index) => {
+    console.log(index + "xde");
+    // var arr = infoArray.list.filter((x) => x != index);
+    // console.log(arr + "xde");
+    // arr.splice(index, 1);
+    //  setInfoArray(arr);
+    setInfoArray([]);
+  };
+
+  function Item({ infoElement, infoIndex }) {
     return (
-      <Swipeable renderLeftActions={LeftActions}>
+      <Swipeable
+        renderLeftActions={LeftActions}
+        onSwipeableLeftOpen={LeftActionOpen}
+        renderRightActions={(progress, dragX) => (
+          <RightActions progress={progress} dragX={dragX} index={infoIndex} />
+        )}
+        onSwipeableRightOpen={(progress, dragX) => (
+          <RightActions progress={progress} dragX={dragX} index={infoIndex} />
+        )}
+      >
         <View>
           <View style={styles.spacer}></View>
           <View style={styles.touchable}>
@@ -53,7 +102,9 @@ export default function (props) {
     <View style={styles.main}>
       <FlatList
         data={infoArray}
-        renderItem={({ item }) => <Item infoElement={item} />}
+        renderItem={({ item, index }) => (
+          <Item infoElement={item} infoIndex={index} />
+        )}
         keyExtractor={(item) => item.id}
       />
     </View>
@@ -93,9 +144,23 @@ const styles = StyleSheet.create({
 
   leftBack: {
     backgroundColor: "yellow",
-    alignItems: "center",
+    borderRadius: 15,
+
+    height: "100%",
+    top: "5%",
+    flex: 1,
+  },
+  leftText: { top: "25%", fontWeight: "600", marginLeft: "5%" },
+  rightBack: {
+    backgroundColor: "red",
+    borderRadius: 15,
+    flex: 1,
     height: "100%",
     top: "5%",
   },
-  leftText: { top: "25%" },
+  rightText: {
+    top: "70%",
+    fontWeight: "900",
+    marginLeft: "75%",
+  },
 });
