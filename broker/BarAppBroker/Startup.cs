71154d437e9fe0beb38cf795d17dff2150ad44aa
z7z53 <64813972+z7z53@users.noreply.github.com>
2020-05-15 18:58:53 +0300

    add empty user controller

--- broker/BarAppBroker/Startup.cs
+++ broker/BarAppBroker/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +12,8 @@ namespace BarAppBroker
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,9 +28,10 @@ namespace BarAppBroker
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Blank page.");
                 });
             });
         }
