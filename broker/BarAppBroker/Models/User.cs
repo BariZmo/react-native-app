03a4d9e7f2f1028ba75f24812ba461843143d5a6
z7z53 <64813972+z7z53@users.noreply.github.com>
2020-05-17 04:52:56 +0300

    add new field to user model

--- broker/BarAppBroker/Models/User.cs
+++ broker/BarAppBroker/Models/User.cs
@@ -7,6 +7,7 @@
         public string Surname { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public bool Blocked { get; set; }
 
         public override string ToString()
         {
