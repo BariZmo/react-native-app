14c861bee1a82aa7fd8ae369a30918f64cebb4c6
z7z53 <64813972+z7z53@users.noreply.github.com>
2020-05-17 04:38:20 +0300

    add lock to http restful operations

--- broker/BarAppBroker/Controllers/UserController.cs
+++ broker/BarAppBroker/Controllers/UserController.cs
@@ -15,6 +15,103 @@ namespace BarAppBroker
     public class UserController : ControllerBase
     {
         const string fileName = "users.json";
+        readonly object _lockObj = new object();
+
+        // GET: api/User
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            lock (_lockObj)
+            {
+                var users = GetUsers().Result;
+                Debug.WriteLine($"HTTP Get. Total: {users.Count}");
+
+                return users;
+            }
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}", Name = "Get")]
+        public User Get(int id)
+        {
+            lock (_lockObj)
+            {
+                var users = GetUsers().Result;
+                Debug.WriteLine($"HTTP Get: {id}. Total: {users.Count}");
+
+                var byId = users.Where(user => user.Id == id);
+                if (byId.Any())
+                {
+                    return byId.First();
+                }
+                else
+                {
+                    return new User()
+                    {
+                        Id = -1
+                    };
+                }
+            }
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public void Post([FromBody] User value)
+        {
+            lock (_lockObj)
+            {
+                var users = GetUsers().Result;
+
+                Debug.WriteLine($"HTTP Post: {value}. Total: {users.Count}");
+
+                if (value.Id <= 1000)
+                {
+                    int maxId = users.DefaultIfEmpty(new User() { Id = 1000 }).Max(user => user.Id) + 1;
+                    value.Id = maxId;
+                }
+
+                if (!users.Any(user => user.Id == value.Id))
+                {
+                    users.Add(value);
+
+                    UpdateUsers(users);
+                }
+            }
+        }
+
+        // PUT: api/User/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] User value)
+        {
+            lock (_lockObj)
+            {
+                var users = GetUsers().Result;
+                Debug.WriteLine($"HTTP Put: {id} {value}. Total: {users.Count}");
+
+                if (users.Remove(users.Single(user => user.Id == id)))
+                {
+                    users.Add(value);
+                }
+
+                UpdateUsers(users);
+            }
+        }
+
+        // DELETE: api/User/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            lock (_lockObj)
+            {
+                var users = GetUsers().Result;
+                Debug.WriteLine($"HTTP Delete: {id}. Total: {users.Count}");
+
+                if (users.Remove(users.Single(user => user.Id == id)))
+                {
+                    UpdateUsers(users);
+                }
+            }
+        }
 
         public static async Task<List<User>> GetUsers()
         {
@@ -39,87 +136,6 @@ namespace BarAppBroker
         {
             string json = JsonSerializer.Serialize(users);
             System.IO.File.WriteAllText(fileName, json);
-        }
-
-        // GET: api/User
-        [HttpGet]
-        public IEnumerable<User> Get()
-        {
-            var users = GetUsers().Result;
-            Debug.WriteLine($"HTTP Get. Total: {users.Count}");
-
-            return users;
-        }
-
-        // GET: api/User/5
-        [HttpGet("{id}", Name = "Get")]
-        public User Get(int id)
-        {
-            var users = GetUsers().Result;
-            Debug.WriteLine($"HTTP Get: {id}. Total: {users.Count}");
-
-            var byId = users.Where(user => user.Id == id);
-            if (byId.Any())
-            {
-                return byId.First();
-            }
-            else
-            {
-                return new User()
-                {
-                    Id = -1
-                };
-            }
-        }
-
-        // POST: api/User
-        [HttpPost]
-        public void Post([FromBody] User value)
-        {
-            var users = GetUsers().Result;
-
-            Debug.WriteLine($"HTTP Post: {value}. Total: {users.Count}");
-
-            if (value.Id <= 1000)
-            {
-                int maxId = users.DefaultIfEmpty(new User() { Id = 1000 }).Max(user => user.Id) + 1;
-                value.Id = maxId;
-            }
-
-            if (!users.Any(user => user.Id == value.Id))
-            {
-                users.Add(value);
-
-                UpdateUsers(users);
-            }
-        }
-
-        // PUT: api/User/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] User value)
-        {
-            var users = GetUsers().Result;
-            Debug.WriteLine($"HTTP Put: {id} {value}. Total: {users.Count}");
-
-            if (users.Remove(users.Single(user => user.Id == id)))
-            {
-                users.Add(value);
-            }
-
-            UpdateUsers(users);
-        }
-
-        // DELETE: api/User/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-            var users = GetUsers().Result;
-            Debug.WriteLine($"HTTP Delete: {id}. Total: {users.Count}");
-
-            if (users.Remove(users.Single(user => user.Id == id)))
-            {
-                UpdateUsers(users);
-            }
         }
     }
 }
