fb537aa256f76f92c08156d43782fb8ff3ebccac
z7z53 <64813972+z7z53@users.noreply.github.com>
2020-05-17 05:12:12 +0300

    add admin model

---
+++ broker/BarAppBroker/Models/Admin.cs
@@ -0,0 +1,14 @@
+﻿namespace BarAppBroker.Models
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id} {Email} {Password}";
+        }
+    }
+}
