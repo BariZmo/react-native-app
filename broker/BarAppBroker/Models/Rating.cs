6414d782870ba9470cc66fce61b9a7236eac1436
z7z53 <64813972+z7z53@users.noreply.github.com>
2020-05-17 05:19:15 +0300

    add missing models

---
+++ broker/BarAppBroker/Models/Rating.cs
@@ -0,0 +1,15 @@
+﻿namespace BarAppBroker.Models
+{
+    public class Rating
+    {
+        public int Score { get; set; }
+        public string Comment { get; set; }
+        public string RatedEntityRole { get; set; }
+        public int RatedEntityId { get; set; }
+
+        public override string ToString()
+        {
+            return $"{RatedEntityId} {RatedEntityRole} {Score} {Comment}";
+        }
+    }
+}
---
+++ broker/BarAppBroker/Models/Visit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BarAppBroker.Models
+{
+    public class Visit
+    {
+        public int UserId { get; set; }
+        public int BarId { get; set; }
+        public List<(string, float)> Items { get; set; }
+
+        public override string ToString()
+        {
+            return $"{UserId} {BarId} {string.Join(" ", Items.Select(item => $"{item.Item1}: {item.Item2}"))}";
+        }
+    }
+}
