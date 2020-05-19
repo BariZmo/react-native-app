9e399f1dab3e84b7c3572c69d8b18054e9cf8e20
z7z53 <64813972+z7z53@users.noreply.github.com>
2020-05-20 00:56:40 +0300

    add rating controllers

---
+++ broker/BarAppBroker/Controllers/BarRatingController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BarAppBroker
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BarRatingController : ControllerBase
+    {
+        // GET: BarRating/5
+        [HttpGet("{id}")]
+        public double Get(int id)
+        {
+            var ratings = new RatingController().Get().Where(rating => rating.RatedEntityRole == "Bar" && rating.RatedEntityId == id);
+            if (ratings.Any())
+                return ratings.Average(rating => rating.Score);
+            else
+                return 0;
+        }
+    }
+}
