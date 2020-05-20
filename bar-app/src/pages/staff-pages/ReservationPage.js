8451ebd40e2ea1cfb9fecc3c27b801af139509a4
z7z53 <64813972+z7z53@users.noreply.github.com>
2020-05-21 01:40:44 +0300

    add approved reservation loading

--- bar-app/src/pages/staff-pages/ReservationPage.js
+++ bar-app/src/pages/staff-pages/ReservationPage.js
@@ -9,16 +9,48 @@ import {
   Alert,
 } from "react-native";
 import {} from "react-native-gesture-handler";
-import { SampleAcceptedReservations } from "./SampleAcceptedReservations";
 import { styles, itemStyles, modalStyles } from "./ReservationListStyles";
 import ReportView from "../../../User/ReportView";
 
 export default function () {
-  const [reservations, setReservations] = useState(SampleAcceptedReservations);
+  const [reservations, setReservations] = useState([]);
   // selectedItem is identified by id
   const [selectedItem, setSelectedItem] = useState(-1);
   const [menuOpen, setMenuOpen] = useState(false);
   const [messagePageOpen, setMessagePageOpen] = useState(false);
+  const [needToLoad, setNeedToLoad] = useState(true);
+
+  // Load reservations from broker
+  let fetchData = () => {
+    fetch("https://barappbroker20200515061143.azurewebsites.net/reservation")
+      .then((response) => {
+        setNeedToLoad(false);
+        console.log("Received approved reservations.");
+        return response.json();
+      })
+      .then((responseJson) => {
+        fetch("https://barappbroker20200515061143.azurewebsites.net/user")
+          .then((response) => response.json())
+          .then((userJson) => {
+            responseJson.forEach((reserv) => {
+              let user = userJson.find((u) => u.id == reserv.userId);
+              reserv.name = user.name;
+              reserv.surname = user.surname;
+              reserv.email = user.email;
+              reserv.number = user.number;
+            });
+
+            setReservations(
+              responseJson.filter(
+                (reservation) =>
+                  reservation.barId == global.loginId &&
+                  reservation.accepted == true
+              )
+            );
+          });
+      });
+  };
+  if (needToLoad) fetchData();
 
   const removeHandler = (id) => {
     setReservations((prevReservations) => {
